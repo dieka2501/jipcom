3674c3e752eb1db66d4da46661e5c048630fe772
Macintosh <darana.blog@gmail.com>
2016-05-24 16:51:20 +0700

    commit

--- app/views/admin/master_member/form.blade.php
+++ app/views/admin/master_member/form.blade.php
@@ -17,142 +17,113 @@
 			<form>
 			<div class="panel-body">
 				<div class="row">
-				<div class="col-md-6">
-					<div class="form-group">
-						<label>Form ID</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Nama Lengkap</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Email</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Password</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Nomor SIM</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Golongan Darah</label>
-						<select class="form-control">
-							<option>A</option>
-							<option>AB</option>
-							<option>B</option>
-						</select>
-					</div>
-					<div class="form-group">
-						<label>Provinsi/Daerah</label>
-						<select class="form-control">
-							<option>Jawa Barat</option>
-						</select>
-					</div>
-					<div class="form-group">
-						<label>Kota</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Kode Pos</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Alamat</label>
-						<textarea class="form-control"></textarea>
-					</div>
-					<div class="form-group">
-						<label>Telepon</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Nomor Polisi</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Nomor Rangka</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Nomor Mesin</label>
-						<input type="text" class="form-control">
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="form-group">
-						<label>Periode Member</label>
-						<select class="form-control">
-							<option>1 Tahun</option>
-						</select>
-					</div>
-					<div class="form-group">
-						<label>Nama Panggilan</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Tempat Tanggal Lahir</label>
-						<div class="row">
-						<div class="col-md-6">
-						<input type="text" class="form-control">
-						</div>
-						<div class="col-md-6">
-						<input type="text" class="form-control">
-						</div>
-						</div>
-					</div>
-					<div class="form-group">
-						<label>Nomor Handphone</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Jenis SIM</label>
-						<select class="form-control">
-							<option>SIM A</option>
-						</select>
-					</div>
-					<div class="form-group">
-						<label>Jenis Kelamin</label>
-						<select class="form-control">
-							<option>Male</option>
-						</select>
-					</div>
-					<div class="form-group">
-						<label>Nama Kantor</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Bidang Usaha</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Jabatan</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Alamat Kantor</label>
-						<textarea class="form-control"></textarea>
-					</div>
-					<div class="form-group">
-						<label>Telepon</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Jenis Kendaraan</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Warna</label>
-						<input type="text" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Tahun Kendaraan</label>
-						<input type="text" class="form-control">
-					</div>
-				</div>
-				</div>
+                    <div class="form-group clearfix">
+                        <!-- <div class="col-md-6">
+                            <label>Registrasi : </label>
+                            <input type="text" class="form-control" placeholder="No : " name="no" />
+                        </div> -->
+                        <div class="col-md-3">
+                            <label>Pendaftaran Anggota</label>
+
+                            <div class="row clearfix">
+                            
+                            <div class="col-md-3">
+                                <div class="radio">
+                                    <label><input type="radio" name="type" class="type" value="new" checked="checked"> Baru</label>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="radio">
+                                    <label><input type="radio" name="type" class="type" value="old"> Ulang</label>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <label>Photo 3x4</label>
+                            <input type="file" name="member_image">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group clearfix">
+                        <div class="col-md-6">
+                            <label>Nama Lengkap</label>
+                            <input type="text" class="form-control" name="member_name" required="required">
+                        </div>
+                        <div class="col-md-6">
+                            <label>Nama Panggilan</label>
+                            <input type="text" class="form-control" name="member_nickname">
+                        </div>
+                    </div>
+                    <div class="form-group clearfix">
+                        <div class="col-md-6">
+                            <label>Tempat / Tgl. Lahir</label>
+                            <input type="text" class="form-control" name="member_birth">
+                        </div>
+                        <div class="col-md-6">
+                            <label>Agama</label>
+                            <input type="text" class="form-control" name="member_religion">
+                        </div>
+                    </div>
+                    <div class="form-group clearfix">
+                        <div class="col-md-6">
+                            <label>Jenis Kelamin</label>
+                            <select class="form-control" name="member_sex"><option value="laki-laki">Laki-laki</option><option value="perempuan">Perempuan</option></select>
+                            <!-- <input type="text" class="form-control" name='member_sex' /> -->
+                        </div>
+                        <div class="col-md-6">
+                            <label>Golongan Darah</label>
+                            <input type="text" class="form-control" name="member_blood_type">
+                        </div>
+                    </div>
+                    <div class="form-group clearfix">
+                        <div class="col-md-12">
+                            <label>Alamat Rumah</label>
+                            <textarea class="form-control" rows="4" name="member_address"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group clearfix">
+                        <div class="col-md-6">
+                            <label>No. Telpon Rumah</label>
+                            <input type="text" class="form-control" name="member_phone">
+                        </div>
+                        <div class="col-md-6">
+                            <label>Alamat Email</label>
+                            <input type="text" class="form-control" name="member_email" required="required">
+                        </div>
+                    </div>
+                    <div class="form-group clearfix">
+                        <div class="col-md-6">
+                            <label>Nama Kantor</label>
+                            <input type="text" class="form-control" name="member_office">
+                        </div>
+                        <div class="col-md-6">
+                            <label>Bidang Usaha</label>
+                            <input type="text" class="form-control" name="member_business">
+                        </div>
+                    </div>
+                    <div class="form-group clearfix">
+                        <div class="col-md-6">
+                            <label>Jabatan</label>
+                            <input type="text" class="form-control" name="member_position">
+                        </div>
+                        <div class="col-md-6">
+                            <label>Alamat Kantor</label>
+                            <input type="text" class="form-control" name="member_office_address">
+                        </div>
+                    </div>
+                    <div class="form-group clearfix">
+                        <div class="col-md-6">
+                            <label>No. Telp. Kantor</label>
+                            <input type="text" class="form-control" name="member_office_number">
+                        </div>
+                        <div class="col-md-6">
+                            <label>No. SIM</label>
+                            <input type="text" class="form-control" name="member_license">
+                        </div>
+                    </div>
+                    <br>
+                </div>
 			</div>
 			<div class="panel-footer">
 				<button type="submit" class="btn btn-default">Approve</button>
